9e3a757ce7787e3164c6305ee16727e7809721a3
simany27 <simany27@gmail.com>
2023-03-25 00:19:36 +0300

    login

--- src/main/java/manage/ApplicationManager.java
+++ src/main/java/manage/ApplicationManager.java
@@ -2,15 +2,21 @@ package manage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class ApplicationManager {
     WebDriver wd;
     UserHelper user;
 
     public void init(){
-        wd = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        wd = new ChromeDriver(options);
         wd.navigate().to("https://trello.com");
         wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         user = new UserHelper(wd);
     }
 
--- src/test/java/tests/Login.java
+++ src/test/java/tests/Login.java
@@ -1,9 +1,18 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Login extends TestBase {
 
     @Test
-    public void login1(){}
+    public void login1() {
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillInLoginForm("simany27@gmail.com", "12345.com");
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+
+        Assert.assertTrue(app.getUser().isLogged());
+    }
 }
